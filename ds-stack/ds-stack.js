28bc5f7e4067b7a05964df7ffc599ab59e287f28
tariq-alhariri <tariqahmad1987@gmail.com>
2017-08-26 09:30:31 -0400

    after adding new code to ds-stack/ds-stack.js

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,17 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage =[]
+    this._last=0
+    this.add = function(value){
       // write me
+      this._storage[this._last]=value
+      this._last+=1
+
     };
     this.remove = function() {
       // write me
+      var del = this._storage[this._last]
+      delete this._storage[this._last]
+      this._last-=1
+      return del 
     };
   };
