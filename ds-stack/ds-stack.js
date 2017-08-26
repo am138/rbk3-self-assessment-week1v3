a9f4b56623aa9c7dd600f205499083c4b24fecb1
Majd Eddin <almuhder@gmail.com>
2017-08-26 10:03:00 +0300

    test

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,11 @@
-var Stack = function() {
+// test
+var Stack = function () {
     this._storage =
-    this.add = function(){
-      // write me
-    };
-    this.remove = function() {
-      // write me
-    };
+        this.add = function () {
+            // write me
+          };
+
+    this.remove = function () {
+        // write me
+      };
   };
