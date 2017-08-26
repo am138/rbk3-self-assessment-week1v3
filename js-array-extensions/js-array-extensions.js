c0857ac51e1379dc0fff273a6016993567040aef
rbkEsraaRamahi <israma7i@yahoo.com>
2017-08-26 02:59:14 -0400

    write the function

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,13 @@
-// your code here
+var Array = function(array){
+	var newObj={};
+	 newObj.first= function(){
+	 	return array[0];
+	 }
+	 newObj.last= function(){
+	 	var x = []
+	 	x = array.pop()
+	 	return x;
+	 }
+	 return newObj;
+}
+	
