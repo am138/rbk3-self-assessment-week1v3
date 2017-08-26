be904ef099cbff78709d2a086b15df07ca4cb684
Haya-AlDineh <haya.aldineh@gmail.com>
2017-08-26 21:45:59 +0300

    Stack Implementation

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,27 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+	var someInstance = {}
+    someInstance.storage = {}
+    someInstance.counter = 0
+
+
+    someInstance.add = function(value){
       // write me
+      //adding data to the Stack:
+      this.storage[this.counter]=value;
+      this.counter ++
+
+      
     };
-    this.remove = function() {
+
+    someInstance.remove = function() {
       // write me
+      // removing data from the Stack/ saving it to give it to give the user 
+	var val = this.storage[this.counter-1]
+	delete this.storage[this.counter-1]
+	this.counter--;
+	return val; 
+
     };
+
+    return someInstance;
   };
