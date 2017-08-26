6aff27e0b56b982ddd7292107fca71c930f9bf48
Siraj Kakeh <sirajonline.89@gmail.com>
2017-08-26 09:52:41 +0300

    All

--- ds-hash-table/ds-hash-table.js
+++ ds-hash-table/ds-hash-table.js
@@ -10,7 +10,24 @@ var makeHashTable = function() {
 
       insert: function(key, value) {
         //your code is here
-        this._storage[hashFn(key, max)] = value;
+        //only add if there's no pre-existance to a value
+        if(this._storage[hashFn(key, max)] === undefined;)
+        {
+          this._storage[hashFn(key, max)] = value;
+        }
+        else
+        {
+          //define a new array
+          var _substorage=[];
+          //store the current value in a temp value
+          var temp=this._storage[hashFn(key, max)];
+          //now make the value equals the new array
+          this._storage[hashFn(key, max)] = _substorage;
+          //now push the old value to it
+          _substorage.push(temp);
+          //push the new value
+          _substorage.push(value);
+        }
     }
   }
 };
