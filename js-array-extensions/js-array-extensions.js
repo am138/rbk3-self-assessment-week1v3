aa1a9381e2a77c3002e9712f86529dbaf5623b06
Abeer-Shaaban <abeer.shaaban2017@gmail.com>
2017-08-26 09:59:54 +0300

    extend arr

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,23 @@
 // your code here
+
+function makeArr(nativeArr){
+	var arrayOfMethod = [];
+	arrayOfMethod[0] = function(){
+		return nativeArr[0];
+	}
+	arrayOfMethod[1] = function(){
+		return nativeArr[nativeArr.length-1];
+	}
+
+	extendArr(nativeArr , arrayOfMethod);
+}
+
+
+
+
+function extendArr(arr1 , arr2){
+	for(var i=0 ; i < arr2.length ; i++){
+		arr1.push(arr2[i]);
+	}
+	return arr1 ;
+}
