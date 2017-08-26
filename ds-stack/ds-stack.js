e0af0ad9e3e3abda84f0955a5020d332e970d021
ramez32 <aboomar3264@gmail.com>
2017-08-26 09:15:40 +0300

    first

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,13 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
-      // write me
+    this._storage =[];
+    this.count=0;
+    this.add = function(value){
+    	count++;
+      this._storage.push(value);
     };
     this.remove = function() {
-      // write me
+    	var x=this._storage[this.count];
+      this._storage.pop();
     };
+    return ._storage;
   };
