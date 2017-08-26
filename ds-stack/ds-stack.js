7922306b1b268675a90ee964af1c57e49d015771
Abeer-Shaaban <abeer.shaaban2017@gmail.com>
2017-08-26 09:54:07 +0300

    stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,14 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage = [];
+    this.add = function(val){
       // write me
+      this._storage.push(val);
     };
     this.remove = function() {
       // write me
+      this._storage.pop();
     };
   };
+
+
+
