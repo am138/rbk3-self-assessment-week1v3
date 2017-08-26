57380bebff6498255ce9c9ddb0cfff23a872613c
mazenalchalah <mazenalchalah@gmail.com>
2017-08-26 09:22:41 +0300

    stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,27 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage ={};
+    this._counter=0;
+    this._add=add;
+    this._remove=remove;
+    this.add = function(value){
+
       // write me
+      this._counter++;//run the counter 
+      this._storage[this._counter]=value;//insert value
+      return this._storage[this._counter];//return the insert value
     };
     this.remove = function() {
       // write me
+      if(this._counter>0)
+      {
+      	var result =this._storage[this._counter];
+      	    delete this._storage[this._counter];
+      	       this._counter--;
+
+      }
+         
+         
+     
+      
     };
   };
