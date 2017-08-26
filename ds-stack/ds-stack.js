1e9a24dfc0c246bbd023717965a329de6a30563c
jwanzerkani94 <zerkani-jwan@hotmail.com>
2017-08-26 02:43:39 -0400

    difference between stack and queue

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,12 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage =[]
+    this.add = function(value){
       // write me
+      this._storage.push(value)
     };
     this.remove = function() {
       // write me
+      var lastIndex = this._storage.length-1;
+      this._storage.splice(lastIndex,lastIndex+1)
     };
   };
