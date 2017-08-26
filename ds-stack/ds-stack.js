545ebfa25ae024a925792be286797c2ea481f7a4
ElectricFiras <ghanem.firas30@gmail.com>
2017-08-26 09:21:18 +0300

    Stack completed

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,13 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage = [];
+    this.add = function(val){
       // write me
+      _storage.push(val)
     };
     this.remove = function() {
       // write me
+      _storage.splice(_storage.length-1 ,1)
     };
   };
+
+  //Completed
