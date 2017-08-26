67bc5b804bb67a1650250c58ad2155a88b596241
AsharDweedar <ashar.dweedar@gmail.com>
2017-08-26 09:29:51 +0300

    finish stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,13 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage = [];
+    this.add = function(val){
+    	this._storage[this._storage.length] = val;
       // write me
     };
     this.remove = function() {
+    	var v = this._storage[this._storage.length - 1];
+    	delete this._storage[this._storage.length - 1];
+    	return v;
       // write me
     };
   };
