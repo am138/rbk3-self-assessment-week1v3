c99c3cb0b27640a407fb9129bc92a268a38e7fb6
Hananmajali <hananmajali@gmail.com>
2017-08-26 10:07:04 -0400

    1st commit

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,16 @@
-// your code here
+ function first(array,n){
+ 	if(n===undefined){
+ 		return array[0]
+ 	}
+ 	return array.slice(0, n);
+ }
+
+ function last(array,n){
+ 	var a= array.length-1
+ 	if(n=== undefined){
+ 		return array[a];
+ 	}
+ 	 return array.slice(-n, array.length)
+ 	
+ }
+  
