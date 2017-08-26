a3993cc21e041dbe9bbba647287b4662c8b14430
Ahmad <eng.ahma1983@gmail.com>
2017-08-26 09:54:57 +0300

    array.first  .last

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,15 @@
 // your code here
+var array=function () {
+	var arr=[];
+	
+	arr.first=function(){
+		return arr[0];
+	}
+	arr.last=function(){
+		return arr[arr.length-1];
+	}
+	return arr;
+
+	// body...
+
+}
