b90cdda73fd3644e477b9b50967a85d2294984b5
Esam <esamsharaf91@gmail.com>
2017-08-26 09:35:09 +0200

    second commit

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,25 @@
-// your code here
+var Array = function(array){ 
+
+newArr = {}; 
+
+newArr._value = array;
+
+newArr._first = function(){ 
+	
+	firstele = this._value[0];
+
+	delete this._value[0];
+
+	this._value.shift();
+
+	return firstele;
+}
+
+newArr._last = function(){ 
+	
+	return this._value.pop();
+
+} 
+
+return newArr;
+}
