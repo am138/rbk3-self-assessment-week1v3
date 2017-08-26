e604ac9ef9fab9f0f45d4c2b263bac3969876114
kamellaba <kamelallababidi@gmail.com>
2017-08-26 10:11:32 +0300

    extention done

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,15 @@
+//2: mostly completed
 // your code here
+function Instantiation_Patterns (){
+	var instance={};
+	instance.array=[1,2,3];
+	._extend(instance,methods)
+	return instance;
+}
+var methods={};
+methods.first=function(){
+	return this.array.unshift();
+}
+methods.last=function(){
+	return this.array.pop();
+}
