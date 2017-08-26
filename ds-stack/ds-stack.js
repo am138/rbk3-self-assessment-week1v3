d3b73fc16a3c6c1c2863dcc71cd5aeec9c045b1d
Esam <esamsharaf91@gmail.com>
2017-08-26 09:20:33 +0200

    first commit

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,16 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
-      // write me
+    this._storage = [];
+
+    this._last = 0;
+
+    this.add = function(value){
+      this._storage[._last++] = value; 
+
     };
     this.remove = function() {
-      // write me
+      if(._last){ 
+      	this.storage[._last].pop();
+      	._last--;
+      }
     };
   };
