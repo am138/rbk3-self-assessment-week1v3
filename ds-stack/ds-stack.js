be33256ce1b7e5cc7f8a9377db75fdcbf5e103c6
RBK5 <rbk5@RBK5s-Mac-mini.local>
2017-08-26 02:39:06 -0400

    Stack alredy done

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,11 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage =[];
+    this.add = function(value){
       // write me
+      this._storage.push(value);
     };
     this.remove = function() {
       // write me
+      return this._storage.pop();
     };
   };
