710f5317085035e98580eb217230aef72788ee2c
Ahmad <eng.ahma1983@gmail.com>
2017-08-26 09:15:30 +0300

    ds stack.js

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,15 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage =[]
+    this.add = function(val){
+    	var s=0;
+      this._storage[s]=val;
+      s++;
       // write me
     };
     this.remove = function() {
+    	var re=this._storage[s];
+    	delete this._storage[s]
+    	s--;
       // write me
     };
   };
