160d01d0522ef2a7aa0b381c635bff049e8440bc
RafatAssaf <gamezst@gmail.com>
2017-08-26 09:33:25 +0300

    initial status

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,9 +1,13 @@
 var Stack = function() {
-    this._storage =
-    this.add = function(){
+    this._storage = [];
+    this.add = function(value){
       // write me
+      //adds to the end of the stack (top)
+      this._storage.push(value);
     };
     this.remove = function() {
       // write me
+      //removes from the end of the stack (pop the top!)
+      return this._storage.pop();
     };
   };
